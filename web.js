d86f6bd869edc744eb9dfdbbf90220bb8177580f
Ubuntu <ubuntu@ip-172-31-28-213.us-west-2.compute.internal>
2013-08-03 22:31:24 +0000

    Added Hello From index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "./index.html";
+var out = fs.readFileSync(htmlfile,'ascii');
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Mohannad ghinajo !');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
